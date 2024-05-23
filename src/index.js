b5fa9110fe0cf217c6baecb82522226e89d8a7bc
Hidekashi <hideaki.lawliet@proton.me>
2024-05-23 19:25:33 +0200

    ADD {function to add task}

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
+
+window.onload = function () {
+    var pageTitle = document.title;
+    var attentionMessage = 'Come back here and focus ';
+
+    document.addEventListener('visibilitychange', function () {
+        document.title = document.hidden ? `${attentionMessage} 😡` : pageTitle;
+    });
+}
+
 const title = document.getElementById("title");
 const titleText = "ToDo List";
 
@@ -28,3 +38,42 @@ function toggleTextVisibility(element, text, duration = 200) {
 }
 
 toggleTextVisibility(title, titleText);
+
+// * Retrieve ToDo from local storage
+
+let todo = JSON.parse(localStorage.getItem("todo"));
+
+// * Add new ToDo
+
+const addTodo = document.getElementById('addBtn');
+addTodo.addEventListener('click', function () {
+    const input = document.getElementById('todo_text');
+    const todo = input.value;
+    if (todo === '' || todo.replace(/\s/g, "" == '')) {
+        input.value = '';
+        window.alert('Please enter a task');
+        return;
+    }
+    input.value = '';
+    console.log(todo);
+    localStorage.setItem('todo', JSON.stringify(todo));
+    const li = document.createElement('li');
+    document.getElementById('todo_list').appendChild(li);
+    li.innerHTML = todo;
+});
+
+// * Delete ToDo
+
+// const deleteTodo = document.getElementById('delete-todo');
+// deleteTodo.addEventListener('click', function () {
+//     const input = document.getElementById('todo-input');
+//     const todo = input.value;
+//     input.value = '';
+//     if (todo) {
+//         const index = todo.indexOf(todo);
+//          todo.splice(index, 1);
+//         localStorage.setItem('todo', JSON.stringify(todo));
+//         const li = document.getElementById(todo);
+//         li.parentNode.removeChild(li);
+//     }
+// });
