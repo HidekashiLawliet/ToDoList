d6be7a7fc9e0bb8ff32a3099655380b1d266825c
Hidekashi <hideaki.lawliet@proton.me>
2024-05-23 18:30:12 +0200

    ADD {JS function typewrite text}

---
+++ src/index.js
@@ -0,0 +1,30 @@
+const title = document.getElementById("title");
+const titleText = "ToDo List";
+
+function toggleTextVisibility(element, text, duration = 200) {
+    let i = 0;
+    let timer;
+
+    function typingEffect() {
+        if (i !== text.length) {
+            element.innerHTML += text.charAt(i);
+            i++;
+            timer = setTimeout(typingEffect, 150);
+        } else {
+            timer = setTimeout(reverseTypingEffect, duration);
+        }
+    }
+
+    function reverseTypingEffect() {
+        if (i !== 1) {
+            element.innerHTML = text.substring(0, i - 1);
+            i--;
+            timer = setTimeout(reverseTypingEffect, 150);
+        } else {
+            timer = setTimeout(typingEffect, duration);
+        }
+    }
+    typingEffect();
+}
+
+toggleTextVisibility(title, titleText);
