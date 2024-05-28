d19a52e49145c9f2114a9c044d3aaf0cbf846c7f
Hidekashi <hideaki.lawliet@proton.me>
2024-05-28 02:15:32 +0200

    ADD {fonction to delete and chcked todo}

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 
-totalTasks = 0;
-
 window.onload = function () {
     var pageTitle = document.title;
     var attentionMessage = 'Come back here and focus ';
@@ -44,6 +42,7 @@ toggleTextVisibility(title, titleText);
 const addTodo = document.getElementById('addBtn');
 let todo = JSON.parse(localStorage.getItem("todo"));
 let count = 0;
+let totalTasks = 0;
 
 function addItemInList() {
     const input = document.getElementById('todo_text');
@@ -56,38 +55,35 @@ function addItemInList() {
     input.value = '';
     console.log(todo);
     localStorage.setItem('todo', JSON.stringify(todo));
-    const buttonDiv = document.createElement('div');
     const li = document.createElement('li');
-    document.getElementById('scroll_list').appendChild(buttonDiv);
-    buttonDiv.className = 'buttonDiv';
     document.getElementById('scroll_list').appendChild(li);
     li.className = count;
     li.innerHTML = `${todo}`;
-    // buttonDiv.innerHTML = `<button class="completed_btn" onClick="toggleCompleted(this)">✔: mark task as completed</button><button class="delete_btn" onClick="deleteItem(this)">X: delete task</button>`;
-    // li.innerHTML = `<input type="checkbox" id="taskCheck" onClick="toggleCompleted(this)"> ${todo}`;
     li.innerHTML = `
-    <input type="checkbox" id="todo" name="todo" value="todo">
-    <label for="todo" data-content="Get out of bed">Get out of bed</label>    
-    `
-
-
+        <input type="checkbox" id="todo" name="todo" value="todo">
+        <label for="todo" data-content="${todo}">${todo}</label>
+        `
+    const deleteBtn = document.createElement('button');
+    li.appendChild(deleteBtn);
+    deleteBtn.id = 'delete_btn';
+    deleteBtn.textContent = 'X';
+    deleteBtn.addEventListener('click', function () {
+        deleteItem(this);
+    });
+    console.log(li.appendChild);
 
     totalTasks += 1;
     ItemCounter(totalTasks);
 }
 
-function toggleCompleted(element) {
-    let enable = false;
-    if (element.checked) {
-        console.log('task completed');
-        element.parentNode.className = 'completed';
-    } else {
-        console.log('task not completed');
-    }
+function deleteItem(element) {
+    totalTasks -= 1;
+    element.parentNode.parentNode.removeChild(element.parentNode);
+    ItemCounter(totalTasks);
 }
 
-
 project_counter = document.getElementById('project_counter');
+
 function ItemCounter(nb) {
     let total = 0;
     total += nb;
@@ -105,19 +101,3 @@ window.addEventListener('keydown', function (event) {
     }
     return;
 });
-
-// * Delete ToDo
-
-// const deleteTodo = document.getElementById('delete_btn');
-// deleteTodo.addEventListener('click', function () {
-//     const input = document.getElementById('todo-input');
-//     const todo = input.value;
-//     input.value = '';
-//     if (todo) {
-//         const index = todo.indexOf(todo);
-//         todo.splice(index, 1);
-//         localStorage.setItem('todo', JSON.stringify(todo));
-//         const li = document.getElementById(todo);
-//         li.parentNode.removeChild(li);
-//     }
-// });
